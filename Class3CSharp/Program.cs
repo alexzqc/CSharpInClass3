7462cc5f42931606406b2812296dd506e8bd1d54
Alex Chen <alexziqic@gmail.com>
2019-11-14 10:05:39 -0800

    Add project files.

---
+++ Class3CSharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace Class3CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseUrl = "https://swapi.co/api/";
+            string planets = "planets/";
+            try
+            {
+                Uri uri = new Uri(baseUrl + planets);
+
+                // Create a web request for the given uri
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                // Get the web response from the api
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                // Get a stream to read the reponse
+                StreamReader responseStream = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                // Read the response and write it to the console
+                Console.WriteLine(responseStream.ReadToEnd());
+                // Close the connection to the api and the stream reader
+                response.Close();
+                responseStream.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error has occured. Could not get planets:\n" + e.Message);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
